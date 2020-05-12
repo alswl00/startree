9ea94e98e3499fa0274e2859be60a1058473f856
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-05-12 12:33:05 +0900

    반복문을 이용한 별찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            int num = 8, i, j, k;
+            for(i = 1; i <= num; i++)
+            {
+                for(j = num - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i * 2 - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
